ee85c6f0662a7b79e5244078fc18f7f016e591a5
Rebeca <rebecasnascimento018@gmail.com>
2024-03-03 17:44:05 -0400

    Commit estilização da página not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,8 +4,9 @@ import styles from "./notFound.module.css"
 export default function NotFound(){
     return(
         <div className={styles.error}>
-            <h2>Error!</h2>
-            <Link href="/">Retornar para Home</Link>
+            <h2 className={styles.h2}>Error!</h2>
+            <h2 className={styles.h2}>Essa página não existe :(</h2>
+            <a href="/"><button className={styles.botao}>Retornar para Home</button></a>
         </div>
     )
 }
