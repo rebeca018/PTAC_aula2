74ce0dab82e62c75edca45beba0849cd051f7b3d
Rebeca <rebecasnascimento018@gmail.com>
2024-03-03 17:56:09 -0400

    Commit acrescentando algumas coisas no CSS

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound(){
     return(
         <div className={styles.error}>
             <h2 className={styles.h2}>Error!</h2>
-            <h2 className={styles.h2}>Essa página não existe :(</h2>
+            <h2 className={styles.h2}>Page not found :(</h2>
             <a href="/"><button className={styles.botao}>Retornar para Home</button></a>
         </div>
     )
