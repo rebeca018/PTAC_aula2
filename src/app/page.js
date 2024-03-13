852cd9444bc8def511bffbb257a3d582ee3f672c
Rebeca018 <rebecasnascimento018@gmail.com>
2024-03-13 14:06:28 -0400

    commit criando página de css para home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import styles from "./home.module.css";
+
 const url = "http://localhost:3000/api";
 
 export default async function Home() {
 // fetch não é algo exclusivo do next, vai fazer a solicitação
   const resposta = await fetch(url,{
-    cache:"no-cache"
+    method: "GET"
   });
   const campus = await resposta.json();
  
