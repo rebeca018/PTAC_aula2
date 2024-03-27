516108b0048a3e4e123d88079b35689f1e3eb480
rebeca018 <rebecasnascimento018@gmail.com>
2024-03-27 13:58:13 -0400

    Commit aula 27-03

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
+'use server'
 import styles from "./home.module.css";
 
-const url = "https://back-end-ifms2.vercel.app/";
+const url = "https://back-end-ifms2.vercel.app/campi";
 
 export default async function Home() {
 // fetch não é algo exclusivo do next, vai fazer a solicitação
   const resposta = await fetch(url,{
     next: {
       revalidate: 1
-    }
+    },
+    method: "GET",
+    headers: {'Content-Type' : 'aplication/json'}
   });
   const campi = await resposta.json();
  
